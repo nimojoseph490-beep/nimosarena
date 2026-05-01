c9b305af1c4d11c4124d73ef3441f0b22059c37c
nimojoseph490-beep <nimojoseph490@gmail.com>
2026-05-01 12:06:11 +0000

    deleted callback

--- nimosarena.py
+++ nimosarena.py
@@ -218,6 +218,18 @@ def recent_orders():
     # This pulls directly from the 'memory_orders' list defined at the top
     return render_template_string(RECENT_ORDERS_HTML, orders=memory_orders[::-1])    
 
+SUCCESS_PAGE = """
+<div style="font-family: sans-serif; text-align: center; padding: 50px;">
+    <h2 style="color: green;">Payment Successful!</h2>
+    <p>Your order has been received and is being processed.</p>
+    <br>
+    <a href="/recent-orders" style="display: inline-block; padding: 12px 24px; background-color: #00bbff; color: white; text-decoration: none; border-radius: 5px; font-weight: bold;">
+        View Recent Orders
+    </a>
+    <p style="margin-top: 20px;"><a href="/">Return Home</a></p>
+</div>
+"""
+
 RECENT_ORDERS_HTML = """
 <!DOCTYPE html>
 <html>
@@ -258,16 +270,6 @@ RECENT_ORDERS_HTML = """
 </body>
 </html>
 """
-
-@app.route('/callback')
-def callback():
-    return """
-    <div style="text-align: center; padding-top: 100px; font-family: sans-serif;">
-        <h1 style="color: green;">Payment Sent!</h1>
-        <p>We have received your request. Your bundle will be sent shortly.</p>
-        <a href="/">Back to Home</a>
-    </div>
-    """
 
 @app.route('/admin')
 def admin():
